ab4fbd236d7ae8c36e18b38fff056f52cb1a0e2a
blulila <mgparkcs@gmail.com>
2021-07-29 02:53:36 +0900

    before authorization

--- 2. Photo_Gallery/Photo_Gallery/ViewController.swift
+++ 2. Photo_Gallery/Photo_Gallery/ViewController.swift
@@ -37,6 +37,39 @@ class ViewController: UIViewController {
         refreshItem.tintColor = .purple.withAlphaComponent(0.3)
     }
     
+    @objc func checkPermission(){
+        if PHPhotoLibrary.authorizationStatus() == .authorized || PHPhotoLibrary.authorizationStatus() == .limited{
+            DispatchQueue.main.async {
+                self.showGallery()
+            }
+            
+        }else if PHPhotoLibrary.authorizationStatus() == .denied{
+            DispatchQueue.main.async {
+                self.showAuthorizationDeniedAlert()
+            }
+            
+        }else if PHPhotoLibrary.authorizationStatus() == .notDetermined{
+            PHPhotoLibrary.requestAuthorization {status in
+                self.checkPermission() //Info.plist - request
+            }
+        }
+    }
+    
+    func showAuthorizationDeniedAlert(){
+        let alert = UIAlertController(title: "포토라이브러리 접근 권한을 활성화 해주세요.", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "닫기", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "설정으로 가기", style: .default, handler: {
+            action in
+            guard let url = URL(string: UIApplication.openSettingsURLString) else{
+                return
+            }
+            if UIApplication.shared.canOpenURL(url){
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     @objc func showGallery(){
         let library = PHPhotoLibrary.shared()
         
@@ -45,7 +78,6 @@ class ViewController: UIViewController {
         
         let picker = PHPickerViewController(configuration: configuration)
         picker.delegate = self
-
         present(picker, animated: true)
     }
     
